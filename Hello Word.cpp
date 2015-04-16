979ddb9be4e31202b1f342b8779ca44961870697
IvanHeimsath <ivan_heimsath@hotmail.com>
2015-04-16 01:06:12 -0300

    Update and rename Hello Word -w- ios to Hello Word.cpp

---
+++ Hello Word.cpp
@@ -0,0 +1,14 @@
+/*
+	Autor: Heimsath, Ivan Ezequiel
+	Curso: K1091
+	Descripción: TP#0 -"Hello World"-
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello little big world!\n" << endl;
+}
